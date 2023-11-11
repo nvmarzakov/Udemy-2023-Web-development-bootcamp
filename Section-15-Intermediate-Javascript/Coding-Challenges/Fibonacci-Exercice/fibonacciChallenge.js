fa08fe93d44edc4e5944d4e44d1012badbfd8efd
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2023-11-11 02:43:22 +0200

    coding exercise

---
+++ Section-15-Intermediate-Javascript/Coding-Challenges/Fibonacci-Exercice/fibonacciChallenge.js
@@ -0,0 +1,38 @@
+function fibonacciGenerator (n) {
+    //Do NOT change any of the code above 👆
+        
+        //Write your code here:
+        var output = [];
+        
+        if (n === 1) {
+            output.push(0);
+        } else if ( n === 2) {
+            output.push(0, 1);
+        } else {
+            output = [0, 1];
+            for (let i = 2; i < n; i++) {
+                let lastNumberIndex = (output.length - 1);
+                let lastNumber = output[lastNumberIndex];
+                let previousNumber = output[lastNumberIndex - 1];
+                let nextNumber = previousNumber + lastNumber;
+                output.push(nextNumber);
+            }
+
+        }
+        
+        
+        console.log(output)
+        
+        
+        
+        
+        
+        
+        //Return an array of fibonacci numbers starting from 0.
+        
+    //Do NOT change any of the code below 👇
+    }
+
+    fibonacciGenerator(7)
+    
+    
