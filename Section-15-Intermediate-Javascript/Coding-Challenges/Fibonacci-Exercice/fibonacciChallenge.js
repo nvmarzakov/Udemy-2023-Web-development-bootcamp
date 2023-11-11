14e3b4e9c944a5bd8837c6e9f5201a00989ab2b8
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2023-11-11 02:44:51 +0200

    tabspace

--- Section-15-Intermediate-Javascript/Coding-Challenges/Fibonacci-Exercice/fibonacciChallenge.js
+++ Section-15-Intermediate-Javascript/Coding-Challenges/Fibonacci-Exercice/fibonacciChallenge.js
@@ -1,38 +1,37 @@
-function fibonacciGenerator (n) {
+function fibonacciGenerator(n) {
     //Do NOT change any of the code above 👆
-        
-        //Write your code here:
-        var output = [];
-        
-        if (n === 1) {
-            output.push(0);
-        } else if ( n === 2) {
-            output.push(0, 1);
-        } else {
-            output = [0, 1];
-            for (let i = 2; i < n; i++) {
-                let lastNumberIndex = (output.length - 1);
-                let lastNumber = output[lastNumberIndex];
-                let previousNumber = output[lastNumberIndex - 1];
-                let nextNumber = previousNumber + lastNumber;
-                output.push(nextNumber);
-            }
 
+    //Write your code here:
+    var output = [];
+
+    if (n === 1) {
+        output.push(0);
+    } else if (n === 2) {
+        output.push(0, 1);
+    } else {
+        output = [0, 1];
+        for (let i = 2; i < n; i++) {
+            let lastNumberIndex = (output.length - 1);
+            let lastNumber = output[lastNumberIndex];
+            let previousNumber = output[lastNumberIndex - 1];
+            let nextNumber = previousNumber + lastNumber;
+            output.push(nextNumber);
         }
-        
-        
-        console.log(output)
-        
-        
-        
-        
-        
-        
-        //Return an array of fibonacci numbers starting from 0.
-        
-    //Do NOT change any of the code below 👇
+
     }
 
-    fibonacciGenerator(7)
-    
-    
+
+    console.log(output)
+
+
+
+
+
+
+    //Return an array of fibonacci numbers starting from 0.
+
+    //Do NOT change any of the code below 👇
+}
+
+fibonacciGenerator(7)
+
