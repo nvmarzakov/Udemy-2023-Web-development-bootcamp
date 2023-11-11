dc75c1a43327dfa8705a574f1912a3a987517550
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2023-11-11 15:17:46 +0200

    update

--- Section-17-The-Dice-Challenge/Dicee+Challenge+-+Starting+Files/Dicee Challenge - Starting Files/index.js
+++ Section-17-The-Dice-Challenge/Dicee+Challenge+-+Starting+Files/Dicee Challenge - Starting Files/index.js
@@ -1,19 +1,26 @@
-var randomNumber1 = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-var randomNumber2 = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+// funcion return random number between 1 to 6 
+function randomNumber () {
+    return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+}
 
-console.log(randomNumber1)
+// random numbers for players
+var randomNumber1 = randomNumber();
+var randomNumber2 = randomNumber();
 
+// change path to images with refreshing page
 document.querySelector("img.img1").setAttribute("src", `images/dice${randomNumber1}.png`);
 document.querySelector("img.img2").setAttribute("src", `images/dice${randomNumber2}.png`);
 
+
+// if player one win
 if (randomNumber1 > randomNumber2) {
 
     document.querySelector("h1").textContent = "🚩Player 1 Wins!"
-
+// if player two win
 } else if (randomNumber1 < randomNumber2) {
 
     document.querySelector("h1").textContent = "Player 2 Wins!🚩"
-
+// if draw
 } else {
     document.querySelector("h1").textContent = "Draw!"
-}
+} 
