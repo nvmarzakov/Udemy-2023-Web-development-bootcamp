57d01570ec4db45699dba54010c0a82cb855c18d
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2024-03-27 00:00:23 +0200

    Add eventListener with jQuery

--- Section 19 JQuery/jQuery/index.js
+++ Section 19 JQuery/jQuery/index.js
@@ -1,3 +1,24 @@
-$("h1").addClass("big-title margin-50");
+// $("h1").click(function() {
+//     $("h1").css("color", "purple");
+// })
 
-$("button")
+// for (let index = 0; index < 5; index++) {
+//     document.querySelectorAll("button")[i].addEventListener("click", function() {
+//         document.querySelector("h1").style.color = "purple";
+//     })
+    
+// }
+
+// $("button").click(function() {
+//     $("h1").css("color", "purple");
+// })
+
+
+//Manipulate Text 
+// $(document).keypress(function(event) {
+//     $("h1").text(event.key);
+// })
+
+$("h1").on("click", function() {
+    $("h1").css("color", "purple");
+})
